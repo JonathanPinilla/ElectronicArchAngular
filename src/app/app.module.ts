6cd25190350d8911041cde326ac38573c1679576
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-10 14:26:22 -0500

    grid adapted to items service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatIconModule} from "@angular/material/icon";
 import {NgOptimizedImage} from "@angular/common";
 import { GridComponent } from './components/grid/grid.component';
 import {MatCardModule} from "@angular/material/card";
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import {MatCardModule} from "@angular/material/card";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatFormFieldModule,
     MatInputModule,
     MatToolbarModule,
--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItemsService } from 'src/app/services/items.service';
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss']
 })
-export class GridComponent {
+export class GridComponent implements OnInit{
+
+  items: any[] = [];
+
+  constructor(private service: ItemsService){}
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(){
+    return this.service.getAll().subscribe({
+      next: items => {
+        this.items = items;
+      },
+      error:error => console.error(error)
+    });
+  }
 
 }
