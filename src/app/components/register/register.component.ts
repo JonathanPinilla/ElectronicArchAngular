5127faff84fdc410a651449326c6f96e409353c8
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-12 15:31:59 -0500

    Register component, design and form control

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  constructor(
+    private builder: FormBuilder,
+  ) {
+  }
+
+  registerForm = this.builder.group({
+    email: '',
+    password: ''
+  });
+
+}
