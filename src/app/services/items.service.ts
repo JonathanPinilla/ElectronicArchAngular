a5f3a206bf2f52b82ca175bf944911fb8affca58
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-13 14:02:11 -0500

    Items service general methods updated

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import {Item} from "../models/item";
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +15,21 @@ export class ItemsService {
   getAll(): Observable<any>{
     return this.http.get(this.api);
   }
+
+  getOne(id: string): Observable<any>{
+    return this.http.get(`${this.api}/${id}`);
+  }
+
+  save(item: Item): Observable<Item>{
+    return this.http.post<Item>(this.api, item);
+  }
+
+  update(item: Item): Observable<Item>{
+    return this.http.put<Item>(`${this.api}/${item.id}`, item);
+  }
+
+  delete(id: string): Observable<any>{
+    return this.http.delete(`${this.api}/${id}`);
+  }
+
 }
