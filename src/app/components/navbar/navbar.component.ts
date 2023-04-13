6cc5bc1025da8b1876a20e58ab403ae89c61ad49
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-13 14:00:10 -0500

    Navbar user button

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,22 +1,37 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {ClientService} from "../../services/client.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
-  @Output() toggleSidenavEvent = new EventEmitter();
-
-  toggleSidenav() {
-    this.toggleSidenavEvent.emit();
+  constructor(private clientService: ClientService, private router: Router) {
   }
-
-  showFiller = false;
-
+  logged: string = 'false';
   cartItems = [
     { name: 'Item 1', price: 10.99, image: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Diode-closeup.jpg' },
     { name: 'Item 2', price: 7.99, image: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Diode-closeup.jpg' }
   ];
+
+  ngOnInit(): void {
+    this.logged = localStorage.getItem('logged') || 'false';
+  }
+
+  logout(){
+    this.clientService.logout()
+      .then(() => {
+        console.log('logged out');
+        this.logged = 'false';
+        localStorage.setItem('logged', String(false));
+        this.router.navigate(['']);
+      })
+      .catch((error) => {
+      console.log(error);
+    });
+  }
+
 }
