49261fa7cfe22350ce746c5b5e908ccbae0bc18d
Jonathan Pinilla <jonathan_pinilla@outlook.com>
2023-04-13 09:40:08 -0500

    Client service register form working and saving at the backend database

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,28 +1,36 @@
-import { Component } from '@angular/core';
-import {FormBuilder, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ClientService} from "../../services/client.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
-  constructor(
-    private builder: FormBuilder,
-  ) {
+  registerForm: FormGroup = new FormGroup({})
+
+  constructor(private builder: FormBuilder, private clientService: ClientService, private router: Router) {
   }
 
-  registerForm = this.builder.group({
-    name: ['', Validators.required],
-    lastName: ['', Validators.required],
-    birthDate: ['', [Validators.required, Validators.pattern('^\\d{4}-\\d{2}-\\d{2}$')]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(8)]],
-  });
+  ngOnInit(): void {
+    this.registerForm = this.builder.group({
+      id: '',
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthDate: ['', [Validators.required, Validators.pattern('^\\d{4}-\\d{2}-\\d{2}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      orders: [],
+      deleted: false,
+    });
+  }
 
-  submit(){
-
+  submit() {
+    this.clientService.register(this.registerForm.value).then(response =>
+      this.router.navigate(['/login']));
   }
 
 }
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -13,25 +13,27 @@ export class ClientService {
   constructor(private auth: Auth, private http: HttpClient) {}
 
   register(client: Client){
-    let client2: Client = client;
-    createUserWithEmailAndPassword(this.auth, client.email, client.password)
+
+    return createUserWithEmailAndPassword(this.auth, client.email, client.password)
       .then((userCredential) => {
+
         // Signed in
-        client2.id = userCredential.user.uid;
-        client2.deleted = false;
-        client2.orders = [];
-        // ...
+        client.id = userCredential.user.uid;
+
+        this.http.post(this.api, client).subscribe({
+            next: response => console.log(response),
+            error: error => console.log(error)
+          });
+
       })
       .catch((error) => {
         console.log(error);
       });
-    return this.http.post(this.api, client2);
+
   }
 
   login({email, password}: any){
     return signInWithEmailAndPassword(this.auth, email, password);
   }
-
-
 
 }
